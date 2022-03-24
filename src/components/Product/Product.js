fde26a174b5468c115feaa3542a579d69352a0cc
Brishti Shil <brishtishil62@gmail.com>
2022-03-24 16:33:14 +0600

    second commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -14,7 +14,7 @@ const Product = (props) => {
                 <p className='product-name'>{name}</p>
                 <p>Price: ${price}</p>
                 <p><small>Seller: {seller}</small></p>
-                <p><small>Ratings: {ratings}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
             </div>
             <button onClick={() => handleAddToClick(product)} className='btn-cart'>
                 <p className='btn-text'>Add to Cart</p>
